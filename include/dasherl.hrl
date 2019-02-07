40e281765d2dc6c6d1cf20c98cabe2690024be7e
zgbjgg <zgbjgg@gmail.com>
2019-02-07 16:48:11 -0600

    fix macros

--- include/dasherl.hrl
+++ include/dasherl.hrl
@@ -1,11 +1,3 @@
 % define for defaults
 -define(DEFAULT_WORKERS, 3).
 -define(DEFAULT_BIND, "127.0.0.1:8000").
--define(GUNICORN, "gunicorn").
-
-% define the macros for CMD building
--define(BIND(Bind), " --bind=" ++ Bind).
--define(WORKERS(Workers), " --workers=" ++ integer_to_list(Workers)).
--define(CHDIR(Chdir), " --chdir=" ++ Chdir).
--define(PIDFILE(UnixPid), " --pid=" ++ UnixPid).
--define(APP, " dasherl:server").
