3f0abf859ece1ec891209e432f03a3c9cda211fa
zgbjgg <zgbjgg@gmail.com>
2019-02-08 12:39:28 -0600

    fix router from py to allow preloaded and not preloaded layouts

--- priv/dasherl_router.py
+++ priv/dasherl_router.py
@@ -8,9 +8,16 @@ from erlport.erlang import call
 # render layout from dasherl_apps configured
 def render_layout(path, dasherl_apps):
     # simply call to erlang module and ask for app rendering that path
-    dapp = call(Atom("dasherl_router"), Atom("render"), [path])
-    mod = getattr(dasherl_apps, dapp, None)
-    if mod is None:
+    dapp, preloaded = call(Atom("dasherl_router"), Atom("render"), [path])
+    # maybe layout cannot from a preloaded mod, its built on the fly by
+    # dasherl erlang side :-)
+    if preloaded == True :
+        mod = getattr(dasherl_apps, dapp, None)
+        if mod is None:
+            return None
+        else:
+            return getattr(mod, 'layout', None)
+    elif dapp == "none":
         return None
     else:
-        return getattr(mod, 'layout', None)
+        return dapp
