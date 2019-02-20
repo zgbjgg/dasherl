b255a3f1f0f808d067ee76c2ded8005643bab586
zgbjgg <zgbjgg@gmail.com>
2019-02-20 12:56:58 -0600

    manage only gunicorn from a separated python process

--- src/dasherl_gunicorn_worker.erl
+++ src/dasherl_gunicorn_worker.erl
@@ -3,13 +3,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/1,
-    stop/1,
-    run_server/1,
-    stop_server/1,
-    setup_route/3,
-    del_route/2,
-    setup_callback/4]).
+-export([start_link/1, stop/1, run_server/1]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -25,28 +19,17 @@
 -record(state, {py_pid = undefined :: pid(),
     mon_ref = undefined,
     workers = 3 :: non_neg_integer(),
-    bind = "127.0.0.1:80" :: string()}).
+    bind = "127.0.0.1:80" :: string(),
+    gunicorn_pid = undefined :: pid()}).
 
 start_link(Settings) ->
     gen_server:start_link(?MODULE, Settings, []).
 
-stop(Pid) ->
-    gen_server:call(Pid, stop_link).
-
 run_server(Pid) ->
     gen_server:call(Pid, run_server).
 
-stop_server(Pid) ->
-    gen_server:call(Pid, stop_server).
-
-setup_route(Pid, Route, Layout) ->
-    gen_server:call(Pid, {setup_route, Route, Layout}).
-
-del_route(Pid, Route) ->
-    gen_server:call(Pid, {del_route, Route}).
-
-setup_callback(Pid, Outputs, Inputs, Lambda) ->
-    gen_server:call(Pid, {setup_callback, Outputs, Inputs, Lambda}).
+stop(Pid) ->
+    gen_server:call(Pid, stop_link).
 
 init(Settings) ->
     process_flag(trap_exit, true),
@@ -56,8 +39,7 @@ init(Settings) ->
     % argument of init gen_server
     Workers = proplists:get_value(workers, Settings, ?DEFAULT_WORKERS),
     Bind = proplists:get_value(bind, Settings, ?DEFAULT_BIND),
-    Stylesheets = proplists:get_value(stylesheets, Settings,
-        ['https://codepen.io/chriddyp/pen/bWLwgP.css']),
+    Stylesheets = proplists:get_value(stylesheets, Settings, ?DEFAULT_STYLESHEETS),
 
     Path = code:priv_dir(dasherl),
 
@@ -65,16 +47,13 @@ init(Settings) ->
     case python:start([{python_path, Path}]) of
         {ok, PyPid} ->
             MonRef = erlang:monitor(process, PyPid),
-            lager:info("initialized default modules for py pid ~p", [PyPid]),
 
-            % initialize decoder for erl dash components
-            ok = python:call(PyPid, dasherl_components, setup_dasherl_components_type, []),
-
-            "ok" = python:call(PyPid, dasherl, initialize, [Workers, list_to_atom(Bind),
-                Stylesheets, list_to_binary(pid_to_list(PyPid))]),
+            % initialize gunicorn with dasherl and hold in a process
+            PidGunicorn = initialize_from_scratch(PyPid, Workers, Bind, Stylesheets),
+            lager:info("gunicorn is up and running at linked process: ~p", [PidGunicorn]),
 
             {ok, #state{py_pid = PyPid, mon_ref = MonRef, workers = Workers,
-                bind = Bind}};
+                bind = Bind, gunicorn_pid = PidGunicorn}};
         Error      ->
             lager:error("cannot initializes py due to ~p", [Error]),
             {stop, Error}
@@ -82,50 +61,16 @@ init(Settings) ->
 
 handle_call(run_server, _From, State) ->
     PyPid = State#state.py_pid,
-    Appid = list_to_binary(pid_to_list(PyPid)),
-    Pid = list_to_binary(pid_to_list(self())),
-    case catch python:call(PyPid, dasherl, run, [Appid, Pid]) of
-        {'EXIT', {{python, Class, Argument, _Stack}, _}} ->
-            {reply, {error, {Class, Argument}}, State};
-        "ok"                                             ->
-            {reply, ok, State}
-    end;
-handle_call(stop_server, _From, State) ->
-    PyPid = State#state.py_pid,
-    Pid = list_to_binary(pid_to_list(self())),
-    case catch python:call(PyPid, dasherl, stop, [Pid]) of
-        {'EXIT', {{python, Class, Argument, _Stack}, _}} ->
-            {reply, {error, {Class, Argument}}, State};
-        "no_proc"                                        ->
-            {reply, {error, no_proc}, State};
-        "ok"                                             ->
-            {reply, ok, State}
-    end;
-handle_call({setup_route, Route, Layout}, _From, State) ->
-    PyPid = State#state.py_pid,
-    case catch python:call(PyPid, dasherl, setup_route, [Route, Layout]) of
-        {'EXIT', {{python, Class, Argument, _Stack}, _}} ->
-            {reply, {error, {Class, Argument}}, State};
-        "ok"                                             ->
-            {reply, ok, State}
-    end;
-handle_call({del_route, Route}, _From, State) ->
-    PyPid = State#state.py_pid,
-    case catch python:call(PyPid, dasherl, del_route, [Route]) of
-        {'EXIT', {{python, Class, Argument, _Stack}, _}} ->
-            {reply, {error, {Class, Argument}}, State};
-        "ok"                                             ->
-            {reply, ok, State}
-    end;
-handle_call({setup_callback, Outputs, Inputs, Lambda}, _From, State) ->
-    PyPid = State#state.py_pid,
-    case catch python:call(PyPid, dasherl, setup_callback, [Outputs, Inputs, Lambda]) of
-        {'EXIT', {{python, Class, Argument, _Stack}, _}} ->
-            {reply, {error, {Class, Argument}}, State};
-        "ok"                                             ->
-            {reply, ok, State}
-    end;
+    Appid = list_to_binary(pid_to_list(self())),
+
+    % from python side this call blocks the entire port, so hold in a separate
+    % process and keep this gen_server normal state
+    PidGunicorn = spawn_link(python, call, [PyPid, dasherl, run, [Appid]]),
+
+    {reply, ok, State#state{gunicorn_pid = PidGunicorn}};
 handle_call(stop_link, _From, State) ->
+    ok = stop_signal(State#state.gunicorn_pid),
+    ok = python:stop(State#state.py_pid),
     {stop, normal, ok, State};
 handle_call(_Request, _From, State) ->    
     {reply, ok, State}.
@@ -140,6 +85,7 @@ handle_info(_Info, State) ->
     {noreply, State}.
 
 terminate(_Reason, State) ->
+    ok = stop_signal(State#state.gunicorn_pid),
     % when finish process just stop py_pid
     ok = python:stop(State#state.py_pid),
     ok.
@@ -150,3 +96,24 @@ code_change(_OldVsn, State, _Extra) ->
 %% ===================================
 %% Internal Funcionts
 %% ===================================
+
+% initialize the app and keep it in python side so we
+% can run after the entire gen_server was initialized correctly 
+initialize_from_scratch(PyPid, Workers, Bind, Stylesheets) ->
+    BindAtom = list_to_atom(Bind),
+    Appid = list_to_binary(pid_to_list(self())),
+    case catch python:call(PyPid, dasherl, initialize, [Workers, BindAtom, Stylesheets, Appid]) of
+        {'EXIT', {{python, Class, Argument, _Stack}, _}} ->
+            {error, {Class, Argument}};
+        "ok"                                             ->
+            ok
+    end.
+
+% since is a blocking process, gunicorn cannot be stopped just with py
+% so stop process linked, after that stop with signal handler using
+% sigterm.
+stop_signal(Pid) ->
+    exit(Pid, kill),
+    [UnixPid|_] = string:tokens(os:cmd("cat " ++ ?DEFAULT_UNIX_PID), "\n"),
+    _ = os:cmd("kill -9 " ++ UnixPid),
+    ok.
