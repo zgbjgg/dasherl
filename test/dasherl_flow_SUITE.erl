edc80a7782b8fa69a71479b7140e584f49d2f05c
zgbjgg <zgbjgg@gmail.com>
2019-02-20 23:46:55 -0600

    fix tests to work with new implementation

--- test/dasherl_flow_SUITE.erl
+++ test/dasherl_flow_SUITE.erl
@@ -4,12 +4,10 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
--export([test_dasherl_setup_route/1,
-    test_dasherl_del_route/1]).
+-export([test_dasherl_compile_mod/1]).
 
 all() ->
-    [test_dasherl_setup_route,
-     test_dasherl_del_route].
+    [test_dasherl_compile_mod].
 
 init_per_testcase(_, _Config) ->
     % use ibrowse
@@ -20,45 +18,24 @@ init_per_testcase(_, _Config) ->
     ok = application:ensure_started(lager),
     ok = application:ensure_started(erlport),
     ok = application:start(dasherl),
+    ok = application:set_env(dasherl, routes, []),
     {ok, Pid} = dasherl_gunicorn_worker:start_link([{bind, "127.0.0.1:8000"},
         {workers, 1}]),
-    ok = dasherl_gunicorn_worker:run_server(Pid),
     [{worker, Pid}].
 
 end_per_testcase(_, Config) ->
     Pid = proplists:get_value(worker, Config),
-    ok = dasherl_gunicorn_worker:stop_server(Pid),
     ok = dasherl_gunicorn_worker:stop(Pid),
     ok = application:stop(dasherl),
     ok.
 
-test_dasherl_setup_route(Config) ->
+test_dasherl_compile_mod(Config) ->
     Pid = proplists:get_value(worker, Config),
 
-    % define a custom layout from erlang
-    Input = dasherl_components:input([{placeholder, 'Write here'}, {value, ''}, {type, 'text'}]),
-    Div = dasherl_components:divc([{children, [Input]}]),
+    % define a custom module and compile using dasherl
+    ok = dasherl:compile(Pid, "/my-dasherl-route", mod_helper),
 
-    % set router to render layout
-    ok = dasherl_gunicorn_worker:setup_route(Pid, <<"/my-dasherl-route">>, Div),
-
-    % use ibrowse to check
-    {ok, Code, _, _} = ibrowse:send_req("http://127.0.0.1:8000/my-dasherl-route", [], get),
-
-    ?assertEqual("200", Code).
-
-test_dasherl_del_route(Config) ->
-    Pid = proplists:get_value(worker, Config),
-
-    % define a custom layout from erlang
-    Input = dasherl_components:input([{placeholder, 'Write here'}, {value, ''}, {type, 'text'}]),
-    Div = dasherl_components:divc([{children, [Input]}]),
-
-    % set router to render layout
-    ok = dasherl_gunicorn_worker:setup_route(Pid, <<"/my-dasherl-route">>, Div),
-
-    % remove route
-    ok = dasherl_gunicorn_worker:del_route(Pid, <<"/my-dasherl-route">>),
+    ok = dasherl_gunicorn_worker:run_server(Pid),
 
     % use ibrowse to check
     {ok, Code, _, _} = ibrowse:send_req("http://127.0.0.1:8000/my-dasherl-route", [], get),
