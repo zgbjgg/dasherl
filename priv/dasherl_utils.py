6797868bb1909fcdaa16e41dafc0302056278bbc
zgbjgg <zgbjgg@gmail.com>
2019-02-22 10:48:12 -0600

    add utils for dasherl

---
+++ priv/dasherl_utils.py
@@ -0,0 +1,18 @@
+# utilities for dasherl
+
+def build_kwargs(keywords):
+    kwargs = dict([ (k, _build_list_kwargs(v)) for (k, v) in keywords ])
+    return kwargs
+
+def _build_list_kwargs(keywords):
+    if isinstance(keywords, list) and are_keywords(keywords) == True:
+        kwargs = [ _build_kwargs(item) for item in keywords ]
+        return kwargs
+    else:
+        return keywords
+
+def are_keywords(keywords):
+    for keyword in keywords:
+        if not isinstance(keyword, tuple) and not len(keyword) == 2:
+            return False
+    return True
