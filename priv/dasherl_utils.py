c56c57e07b10aa806a942c4a852d8df4bed93655
zgbjgg <zgbjgg@gmail.com>
2019-03-19 19:44:29 -0600

    fix utils, use for cases when a list of lists is a keyword value

--- priv/dasherl_utils.py
+++ priv/dasherl_utils.py
@@ -1,22 +1,19 @@
 # utilities for dasherl
 
 def build_kwargs(keywords):
-    if are_keywords(keywords) == True:
-        kwargs = dict([ (k, _build_list_kwargs(v)) for (k, v) in keywords ])
-        return kwargs
+    kwargs = dict([ (k, _check_value(v)) for (k,v) in keywords ])
+    return kwargs
+
+def _check_value(v):
+    if isinstance(v, list):
+      kwargs = [ _build_list_kwargs(item) for item in v ]
+      return kwargs
     else:
-        return keywords
+      return v
 
 def _build_list_kwargs(keywords):
-    if isinstance(keywords, list) and are_keywords(keywords) == True:
-        kwargs = [ build_kwargs(item) for item in keywords ]
+    if isinstance(keywords, list):
+        kwargs = build_kwargs(keywords)
         return kwargs
     else:
         return keywords
-
-def are_keywords(keywords):
-    for keyword in keywords:
-        print(isinstance(keyword, tuple))
-        if not isinstance(keyword, tuple) or not len(keyword) == 2:
-            return False
-    return True
