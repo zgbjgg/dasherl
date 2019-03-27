1c4c46856435e2311cdf83ace787bc29ef753cb5
zgbjgg <zgbjgg@gmail.com>
2019-03-27 14:39:20 -0600

    use callback handler for dataframe and add try statement to avoid worker crash

--- priv/dasherl_callback_handler.py
+++ priv/dasherl_callback_handler.py
@@ -4,6 +4,7 @@
 # imports from erlport
 from erlport.erlterms import Atom
 from erlport.erlang import call
+import pandas as pd
 
 # import utils
 import dasherl_utils
@@ -11,26 +12,33 @@ import dasherl_utils
 def response(key, args):
     """Process the callback to erlang side and wait for response."""
 
-    response = call(Atom("dasherl_binding"), Atom("callback"), [key, args])
+    try:
+        response = call(Atom("dasherl_binding"), Atom("callback"), [key, args])
 
-    # parse response in order to check if comes a figure in it
-    response = _parse_response(response)
+        # parse response in order to check if comes a figure in it
+        response = _parse_response(response)
 
-    # maybe response breaks!
-    if response == 'error_in_response':
+        # maybe response breaks!
+        if response == 'error_in_response':
+            return 'ERROR!'
+        else:
+            return response
+    except BaseException, ex:
+        print(str(ex))
         return 'ERROR!'
-    else:
-        return response
 
 # parse response, for now only figure data are parsed, other
 # data keep in the same format
 def _parse_response(response):
     if isinstance(response, list):
         response = tuple([_parse_response(r) for r in response])
-    if isinstance(response, tuple) and len(response) == 2:
+    if isinstance(response, pd.core.frame.DataFrame):
+        response = response.to_dict("rows")
+    elif isinstance(response, tuple) and len(response) == 2:
         tag = response[0]
         traces = response[1]
         if tag == 'data':
             traces = [ dasherl_utils.build_kwargs(trace) for trace in traces ]
-        response = dict([(response[0], traces)])
+            response = dict([(response[0], traces)])
     return response
+
