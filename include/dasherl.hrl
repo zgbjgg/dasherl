acbd14f7ab561477a92056fa472dd34f4f6b8526
zgbjgg <zgbjgg@gmail.com>
2019-05-29 16:03:50 -0500

    fix unix pid define

--- include/dasherl.hrl
+++ include/dasherl.hrl
@@ -1,5 +1,5 @@
 % define for defaults
 -define(DEFAULT_WORKERS, 3).
 -define(DEFAULT_BIND, "127.0.0.1:8000").
--define(DEFAULT_UNIX_PID, "/tmp/dasherl_gunicorn.pid").
+-define(DEFAULT_UNIX_PID(Self), "/tmp/dasherl_" ++ Self ++ ".pid").
 -define(DEFAULT_STYLESHEETS, ['https://codepen.io/chriddyp/pen/bWLwgP.css']).
