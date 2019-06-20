70b712adfaacafb56db38370f57460173fd66ca1
zgbjgg <zgbjgg@gmail.com>
2019-06-20 12:52:34 -0500

    fix process to assign keywords as dict

--- priv/dasherl_utils.py
+++ priv/dasherl_utils.py
@@ -4,15 +4,19 @@
 # utilities for dasherl
 
 def build_kwargs(keywords):
-    kwargs = dict([ (k, _check_value(v)) for (k,v) in keywords ])
+    kwargs = dict([ (k, _check_value(k, v)) for (k,v) in keywords ])
     return kwargs
 
-def _check_value(v):
+def _check_value(k, v):
     if isinstance(v, list):
-      kwargs = [ _build_list_kwargs(item) for item in v ]
-      return kwargs
+        kwargs = [ _build_list_kwargs(item) for item in v ]
+        # since other args than children should be dicts
+        if k != 'children':
+            return dict(kwargs)
+        else:
+            return kwargs
     else:
-      return v
+        return v
 
 def _build_list_kwargs(keywords):
     if isinstance(keywords, list):
